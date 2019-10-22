25f9e21e4d9bd07a650375b9ff9f82bc7a7111bc
Orlando Blagrove <blagrove87@gmail.com>
2019-10-21 20:18:58 -0400

    "layout of board"

---
+++ tic-tac-toe.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+  var board = document.getElementById('board').children;
+  var c = '';
+  var i;
+  for (i = 0; i < board.length; i++) {
+    c = board[i].classList.add('square');
+    
+  }
+}
+
+var player = []
+player[0] = 'player_one'
+player[1] = 'player_two'
+function play(){
+ var v = document.querySelectorAll('#square div');
+ if(player == 0){
+   v[i].classList.
+ }
+}
+
